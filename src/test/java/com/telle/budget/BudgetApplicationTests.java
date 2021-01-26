98fd6a6c2bdb0c4bd670aa431b1d08b0af09f3a0
Maria Chabanets <maria.chabanets@syncretis.com>
2021-01-26 20:23:56 +0700

    Create a project

---
+++ src/test/java/com/telle/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telle.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
