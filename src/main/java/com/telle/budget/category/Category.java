66d8ccce8260064238d111196feea7aa314d862c
Maria Chabanets <maria.chabanets@syncretis.com>
2021-01-30 22:41:36 +0700

    Add Category with Repository and DataJpaTest

---
+++ src/main/java/com/telle/budget/category/Category.java
@@ -0,0 +1,27 @@
+package com.telle.budget.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = PRIVATE)
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String label;
+}
---
+++ src/test/java/com/telle/budget/category/CategoryRepositoryIT.java
@@ -0,0 +1,116 @@
+package com.telle.budget.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class CategoryRepositoryIT {
+    private static final String LABEL = "CategoryServiceIT label";
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void shouldFindNone() {
+        // when
+        List<Category> actual = categoryRepository.findAll();
+
+        // then
+        assertThat(actual)
+                .usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(List.of());
+    }
+
+    @Test
+    void shouldFindAll() {
+        // given
+        Category category1 = saveCategory();
+        Category category2 = saveCategory();
+
+        // when
+        List<Category> actual = categoryRepository.findAll();
+
+        // then
+        assertThat(actual)
+                .usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(List.of(category1, category2));
+    }
+
+    @Test
+    void shouldFindById() {
+        // given
+        Category category = saveCategory();
+
+        // when
+        Optional<Category> actual = categoryRepository.findById(category.getId());
+
+        // then
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(Optional.of(category));
+    }
+
+    @Test
+    void shouldSave() {
+        // given
+        Category category = createCategory();
+
+        // when
+        Category actual = categoryRepository.save(category);
+
+        // then
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(category);
+    }
+
+    @Test
+    void shouldDeleteById() {
+        // given
+        Category category1 = saveCategory();
+        Category category2 = saveCategory();
+
+        // when
+        categoryRepository.deleteById(category1.getId());
+
+        // then
+        assertThat(testEntityManager.find(Category.class, category1.getId())).isNull();
+        assertThat(testEntityManager.find(Category.class, category2.getId()))
+                .usingRecursiveComparison()
+                .isEqualTo(category2);
+    }
+
+    @Test
+    void shouldDeleteAll() {
+        // given
+        Category category1 = saveCategory();
+        Category category2 = saveCategory();
+
+        // when
+        categoryRepository.deleteAll();
+
+        // then
+        assertThat(testEntityManager.find(Category.class, category1.getId())).isNull();
+        assertThat(testEntityManager.find(Category.class, category2.getId())).isNull();
+    }
+
+    private Category saveCategory() {
+        return testEntityManager.persist(createCategory());
+    }
+
+    private Category createCategory() {
+        return Category.builder()
+                .label(LABEL)
+                .build();
+    }
+}
