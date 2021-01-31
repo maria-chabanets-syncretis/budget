faa83b7a231b91121db3dad79d04abc1b39d9b26
Maria Chabanets <maria.chabanets@syncretis.com>
2021-01-31 20:23:08 +0700

    Add springfox swagger

---
+++ src/main/java/com/telle/budget/configuration/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.telle.budget.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder().title("Budget").build())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.telle.budget"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
