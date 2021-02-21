29e32a1c13d972b4a56d18e1ce243475d83df2c4
Maria Chabanets <maria.chabanets@syncretis.com>
2021-02-21 23:39:41 +0700

    Add hibernate options and set locale to en_UK

--- src/main/java/com/telle/budget/BudgetApplication.java
+++ src/main/java/com/telle/budget/BudgetApplication.java
@@ -3,10 +3,13 @@ package com.telle.budget;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class BudgetApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.UK);
 		SpringApplication.run(BudgetApplication.class, args);
 	}
 
